39e7f6a23442bebbf783108384dd62e9b903e475
BL-HSG <behaviorlab@unisg.ch>
2018-10-05 14:29:10 +0200

    'demo_publicgoods'

---
+++ demo_publicgoods/models.py
@@ -0,0 +1,32 @@
+from otree.api import (
+    models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
+    Currency as c, currency_range
+)
+
+
+author = 'Your name here'
+
+doc = """
+Your app description
+"""
+
+
+class Constants(BaseConstants):
+    name_in_url = 'demo01'
+    players_per_group = 3
+    num_rounds = 1
+    endowment = c(100)
+    multiplier = 1.8
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    total_contribution = models.CurrencyField()
+    individual_share = models.CurrencyField()
+
+
+class Player(BasePlayer):
+    contribution = models.CurrencyField(min=0, max=Constants.endowment)
