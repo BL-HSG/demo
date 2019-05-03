dada1c42378c7f87ee26787dccb1434b1bd550ce
BL-HSG <behaviorlab@unisg.ch>
2019-05-03 11:39:27 +0200

    'updatepaymentinfo'

--- settings.py
+++ settings.py
@@ -16,13 +16,13 @@ SESSION_CONFIGS = [
         'name': 'demo_publicgoods',
         'display_name': "Public Goods",
         'num_demo_participants': 2,
-        'app_sequence': ['demo_publicgoods', 'payment_info'],
+        'app_sequence': ['demo_publicgoods'],
     },
     {
         'name': 'prisoner',
         'display_name': "Prisoner's Dilemma",
         'num_demo_participants': 2,
-        'app_sequence': ['prisoner', 'payment_info'],
+        'app_sequence': ['prisoner'],
     },
 ]
 
